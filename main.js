7a3650bdedad24af9e3ec0aac7e4952fce894ac6
Caslantas <123511257+Caslantas@users.noreply.github.com>
2023-05-03 23:58:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function getInfo() {
+  let input = document.getElementById('input').value
+  let output = document.getElementById('output')
+  let clear = document.getElementById('clear')
+  let textarea = document.getElementById('input')
+  if (input <= 0 || input > 1000) {
+    alert('lütfen istenilen aralıkta sayı giriniz!')
+  } else {
+    output.innerHTML = ''
+    output.style.display = 'block'
+    for (let i = 0; i <= 10; i++) {
+      let result = `${input}*${i}=${input * i}<hr> `
+      output.innerHTML += result
+    }
+    textarea.value = ''
+    clear.style.display = 'block'
+  }
+}
+
+function clearTable() {
+  let output = document.getElementById('output')
+  let clear = document.getElementById('clear')
+  clear.style.display = 'none'
+  output.style.display = 'none'
+}
